1c58884cc7afc899ec02dd07582c71e2858ebc06
pudelzarulem <fedoseev239161@gmail.com>
2026-02-12 01:11:58 +0300

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,23 @@
+// ЭТОТ ФАЙЛ ХРАНИТ ТВОИ КЛЮЧИ
+// Заполни его один раз и залей на GitHub рядом с index.html
+
+window.STRANGER_KEYS = {
+    // 1. Ключ от ImgBB (для загрузки фото)
+    // Вставь внутри кавычек, например: "381d9f..."
+    IMGBB_KEY: "defe2d2fe4be844631b9578e37e009a0",
+
+    // 2. Конфиг от Firebase (для базы данных)
+    // Вставь сюда весь объект, который ты копировал из консоли Firebase
+    FIREBASE_CONFIG: {
+  apiKey: "AIzaSyAwASaR4X7QBHOsMcVDupuDXSGnIH5lnTE",
+  authDomain: "stranger-shop.firebaseapp.com",
+  projectId: "stranger-shop",
+  storageBucket: "stranger-shop.firebasestorage.app",
+  messagingSenderId: "227186405685",
+  appId: "1:227186405685:web:911af3b5d69299bf63dc7c",
+  measurementId: "G-BQ7LTEKQHD"
+},
+    
+    // 3. Ссылка на твой Телеграм-канал (для кнопки Радио)
+    TELEGRAM_CHANNEL_URL: "https://t.me/radioIznanka"
+};
