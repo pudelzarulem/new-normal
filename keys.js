2b200c4f76f0df73c973f5044e01769df48a2c2f
pudelzarulem <fedoseev239161@gmail.com>
2026-02-15 15:33:51 +0300

    Add files via upload

--- keys.js
+++ keys.js
@@ -3,21 +3,25 @@
 
 window.STRANGER_KEYS = {
     // 1. Ключ от ImgBB (для загрузки фото)
-    // Вставь внутри кавычек, например: "381d9f..."
     IMGBB_KEY: "defe2d2fe4be844631b9578e37e009a0",
 
     // 2. Конфиг от Firebase (для базы данных)
-    // Вставь сюда весь объект, который ты копировал из консоли Firebase
     FIREBASE_CONFIG: {
-  apiKey: "AIzaSyAwASaR4X7QBHOsMcVDupuDXSGnIH5lnTE",
-  authDomain: "stranger-shop.firebaseapp.com",
-  projectId: "stranger-shop",
-  storageBucket: "stranger-shop.firebasestorage.app",
-  messagingSenderId: "227186405685",
-  appId: "1:227186405685:web:911af3b5d69299bf63dc7c",
-  measurementId: "G-BQ7LTEKQHD"
-},
+        apiKey: "AIzaSyAwASaR4X7QBHOsMcVDupuDXSGnIH5lnTE",
+        authDomain: "stranger-shop.firebaseapp.com",
+        projectId: "stranger-shop",
+        storageBucket: "stranger-shop.firebasestorage.app",
+        messagingSenderId: "227186405685",
+        appId: "1:227186405685:web:911af3b5d69299bf63dc7c",
+        measurementId: "G-BQ7LTEKQHD"
+    },
     
     // 3. Ссылка на твой Телеграм-канал (для кнопки Радио)
-    TELEGRAM_CHANNEL_URL: "https://t.me/radioIznanka"
+    TELEGRAM_CHANNEL_URL: "https://t.me/radioIznanka",
+
+    // 4. НАСТРОЙКИ РОБОКАССЫ (НОВОЕ!)
+    // Вставь сюда данные из личного кабинета Робокассы -> Настройки -> Технические
+    ROBOKASSA_LOGIN: "IzNanka1",     // Например: StrangerShop
+    ROBOKASSA_PASS1: "OU90DIQ0gdw4ic1|MxAH",           // Пароль #1
+    ROBOKASSA_TEST_MODE: 1                      // 1 = Тестовый режим, 0 = Боевой режим (реальные деньги)
 };
